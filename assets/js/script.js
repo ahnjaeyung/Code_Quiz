2d298b8ace66720d2679f2e6e3d6e3bbf71e09d6
ahnjaeyung <ahn.jaeyung@gmail.com>
2021-12-18 15:58:29 -0600

    basic structure. created some of the starting variables, created timer, added questions, added some basic starter functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,65 @@
 var questions = [
     {
-        q: "",
-        options: ["", "", "", ""],
-        correct: ""
+        q: "Which one of the characters is not in season one?",
+        options: ["Dennis", "Sweet Dee", "Mac", "Frank"],
+        correct: "Frank"
     },
     {
-        q: "",
-        options: ["", "", "", ""],
-        correct: ""
-        }
+        q: "Who does Charlie first tell he has cancer?",
+        options: ["Dennis", "Sweet Dee", "Mac", "Frank"],
+        correct: "Dennis"
+    },
+    {
+        q: "According to Dennis, what does the D in 'D.E.N.N.I.S. System' stand for?",
+        options: ["Decline their advances", "Demonstrate your value", "Don't ask for permission", "Defer emotion"],
+        correct: "Demonstrate your value"
+    },
+    {
+        q: "What area of law does Charlie claim to specialize in?",
+        options: ["Corporate Law", "Criminal Law", "Bird Law", "Maritime Law"],
+        correct: "Bird Law"
+    }
 ]
 
+var startButton = document.querySelector("#startButton");
+var timeEl = document.querySelector("#time");
+var secondsLeft = 5;
+var currentQ = -1;
+
+startButton.addEventListener("click", startGame);
+
+function startGame() {
+    setTime();
+    console.log("starting quiz",{
+        currentQ
+    });
+    displayQuestion(currentQ);
+}
+function endGame() {
+    console.log("game over");
+}
+function displayQuestion(qNumber) {
+    qNumber++
+    console.log("display question" + qNumber);
+}
+function setTime() {
+    var timerInterval = setInterval(function () {
+        secondsLeft--;
+        timeEl.textContent = "Time: " + secondsLeft;
+        if (secondsLeft === 0) {
+            clearInterval(timerInterval);
+            endGame();
+        }
+    }, 1000);
+}
 // Click on Start button
     // Start the timer
     // Display the first question with options 
         // check if index < questions.length
         // create a function with index as an argument
-    // Each option is a button with click event on it // may be use event.target with matches function
+        // function showQuestion(currentQ) {
+        // }
+    // Each option is a button with click event on it // maybe use event.target with matches function
 
 // Stop the timer
     // if timer = 0;
