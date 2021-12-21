c31bb86e5ca6fb67f12f10bdaf064c3c51b18aec
ahnjaeyung <ahn.jaeyung@gmail.com>
2021-12-20 19:41:52 -0600

    removed some empty lines/spaces to clean the code up a little

--- assets/js/script.js
+++ assets/js/script.js
@@ -112,7 +112,6 @@ function endGame() {
     endGameForm.setAttribute("id", "initials");
     quizArea.appendChild(endGameForm);
 
-
     var endGameSubBtn = document.createElement("button");
     endGameSubBtn.addEventListener("click", saveScore);
     endGameSubBtn.textContent = "Submit";
@@ -191,30 +190,4 @@ function setTime() {
             endGame();
         }
     }, 1000);
-}  //end setTime function definition
-
-// Click on Start button
-    // Start the timer
-    // Display the first question with options 
-        // check if index < questions.length
-        // create a function with index as an argument
-        // function showQuestion(currentQ) {
-        // }
-    // Each option is a button with click event on it // maybe use event.target with matches function
-
-// Stop the timer
-    // if timer = 0;
-    // if user completes the quiz
-
-// Find out if user clicked option is correct or not
-    // if correct: display message (for certain time period)
-    // if wrong: display message (for certain time period) and change the timer accordingly
-    // calculate the score (but don't display)
-// index++
-// Display the next question
-
-// Once user finished with last question
-    // stop timer
-    // ask for user initials in a form > take that info and display it with score
-    // save initials and score in local storage
-    // a href will refer to second html file (highscores.html)
+}
